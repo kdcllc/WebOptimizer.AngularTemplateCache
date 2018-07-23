06956e872b7c43c0ff8f44ddca8d4c6de4ef0f8a
kdcllc <info@kingdavidconsulting.com>
2018-07-23 18:08:57 -0400

    update project files

--- Build.ps1
+++ Build.ps1
@@ -24,17 +24,11 @@ function Exec
 
 if(Test-Path .\src\artifacts) { Remove-Item .\src\artifacts -Force -Recurse }
 
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
-$commitHash = $(git rev-parse --short HEAD)
-$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
-$versionSuffix = @{ $true = "--version-suffix=$($suffix)"; $false = ""}[$suffix -ne ""]
 
-echo "build: Package version suffix is $suffix"
-echo "build: Build version suffix is $buildSuffix" 
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet build WebOptimizer.AngularTemplateCache.sln -c Release --version-suffix=$buildSuffix }
+exec { & dotnet build WebOptimizer.AngularTemplateCache.sln -c Release --version-suffix=$revision }
 
 Push-Location -Path .\test\
 
@@ -46,5 +40,5 @@ try {
 
 Pop-Location
 
-exec { & dotnet pack .\src\WebOptimizer.AngularTemplateCache.csproj -c Release -o .\artifacts --include-symbols --no-build $versionSuffix }
+exec { & dotnet pack .\src\WebOptimizer.AngularTemplateCache.csproj -c Release -o .\artifacts --include-symbols --no-build $revision }
 
